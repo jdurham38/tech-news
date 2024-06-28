779d3a9f571100f8d494f3497bc52f8f40d6e40f
Josh <60802210+jdurham38@users.noreply.github.com>
2024-06-28 17:48:46 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Josh",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Full Stack Engineer",
+    bio: "Interested in all things tech",
+    email: "joshswebdevelopment@gmail.com",
+    linkedin: "joshuadurham38",
+    github: "jdurham38",
+    instagram: "j0shdurham",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Josh's Tech News`,
+      href: "https://github.com/jdurham38/tech-news",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "Josh's Tech News",
+    description: "welcome to Josh's Tech News!",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
