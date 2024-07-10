9cbd78d0549434f809203b5ea13b7b156f6046cd
Joshua Durham <jdurham38@gmail.com>
2024-07-09 20:06:30 -0400

    added posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
+import posthog from "posthog-js"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
-
+  posthog.init('phc_Hx2VOCQg5vLEv6FAmUQoBLesHU4uymsgkn0yHXvPKPD',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
   return (
     <>
           <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9456367246304075"
